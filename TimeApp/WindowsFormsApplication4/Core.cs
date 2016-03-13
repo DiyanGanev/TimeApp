054fe3ee27ff2519fa746fef9fb19e85a2731d82
DiyanGanev <Diyan.Ganev@abv.bg>
2016-03-13 23:31:39 +0100

    v 0.01 Beginning
    Foundation

---
+++ TimeApp/WindowsFormsApplication4/Core.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    class Core 
+    {
+        IList<Class1> storage = new List<Class1>();
+        public DateTime starTime;
+        public DateTime endTime;
+        public TimeSpan spendTime;
+
+        public void Start()
+        {
+           
+            starTime = DateTime.UtcNow;
+        }
+        public void End()
+        {
+            endTime = DateTime.UtcNow;
+            spendTime = endTime.Subtract(starTime);
+            storage.Add(new Class1(starTime, endTime, spendTime));
+
+        }
+        public string Info()
+        {
+            string x = "";
+            foreach (Class1 g in storage)
+            {
+             x = g.Call() + "\n";   
+            }
+            return x;
+        }
+public TimeSpan Total()
+        {
+            TimeSpan x = new TimeSpan();
+    foreach (Class1 g in storage)
+            {
+                x += g.spendTime;
+            }
+
+    return x; 
+         
+        }
+
+    }
+}
---
+++ TimeApp/WindowsFormsApplication4/Time.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    class Time
+    {
+        public DateTime starTime;
+        public DateTime endTime;
+        public TimeSpan spendTime;
+        public Time(DateTime s, DateTime e, TimeSpan d)
+        {
+            this.starTime = s;
+            this.endTime = e;
+            this.spendTime = d;
+        }
+        public string Call()
+        {
+            string x;
+            x = "Start:" + starTime.ToString("HH:mm:ss");
+            x += "End" + endTime.ToString("HH:mm:ss");
+            x += "Eclapsed" + spendTime.ToString(@"hh\:mm\:ss");
+            return x;
+        }
+
+    }
+}
